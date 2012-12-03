184e4b22aa60b8f8f076ab7ab6e8839d24f5e2ae
demis27 <demis27@gmail.com>
2012-12-03 09:50:54 +0000

    Add dwarf generator for Service and DAO

--- core/src/main/sql/dataset.sql
+++ core/src/main/sql/dataset.sql
@@ -1,4 +1,4 @@
-insert into user (user_id, creation_date, modification_date, email, firstname, lastname) values ('aze', current_date, current_date, 'aze@email.com', 'A', 'B');
+insert into "user" (user_id, creation_date, modification_date, email, firstname, lastname) values ('aze', current_date, current_date, 'aze@email.com', 'A', 'B');
 
 insert into family_tree (family_tree_id, creation_date, modification_date, user_id) values ('abc', current_date, current_date, 'aze');
 insert into family_tree (family_tree_id, creation_date, modification_date, user_id) values ('def', current_date, current_date, 'aze');
