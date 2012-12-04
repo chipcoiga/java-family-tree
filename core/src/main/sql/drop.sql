f25b0e59f249df85aaf397b59d9d5c33fe3ae4ee
demis27 <demis27@gmail.com>
2012-12-04 15:53:49 +0000

    Add event, date and date container

--- core/src/main/sql/drop.sql
+++ core/src/main/sql/drop.sql
@@ -1,9 +1,8 @@
-drop table family_relation;
-
-drop table family;
-
-drop table individual;
-
-drop table family_tree;
-
-drop table "user";
+drop table if exists "user" cascade;
+drop table if exists "date" cascade;
+drop table if exists date_container cascade;
+drop table if exists event cascade;
+drop table if exists family_relation cascade;
+drop table if exists family cascade;
+drop table if exists individual cascade;
+drop table if exists family_tree cascade;
