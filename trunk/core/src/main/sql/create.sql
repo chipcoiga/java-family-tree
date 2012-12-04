3dc46fc9844ca77e87deaa001697f70570786d6f
demis27@gmail.com <demis27@gmail.com@453960b6-05be-7b13-05d6-f344ed9ac0eb>
2012-12-04 15:53:49 +0000

    Add event, date and date container

--- trunk/core/src/main/sql/create.sql
+++ trunk/core/src/main/sql/create.sql
@@ -64,3 +64,52 @@ create table family_relation (
     constraint family_relation_individual_fk foreign key (individual_id)
         references individual (individual_id)
 );
+
+create table "date" (
+    date_id             character varying(32),
+    family_tree_id      character varying(32),
+    creation_date       timestamp without time zone,
+    modification_date   timestamp without time zone,
+    year                integer,
+    month               integer,
+    day                 integer,
+    hour                integer,
+    minute              integer,
+    second              integer,
+
+    constraint date_pk primary key (date_id),
+    constraint date_container_family_tree_fk foreign key (family_tree_id)
+        references family_tree (family_tree_id)
+);
+
+create table date_container (
+    date_container_id   character varying(32),
+    family_tree_id      character varying(32),
+    creation_date       timestamp without time zone,
+    modification_date   timestamp without time zone,
+    mode                character varying(1),
+    first_date_id       character varying(32),
+    second_date_id      character varying(32),
+
+    constraint date_container_pk primary key (date_container_id),
+    constraint first_date_date_fk foreign key (first_date_id)
+        references "date" (date_id),
+    constraint second_date_date_fk foreign key (second_date_id)
+        references "date" (date_id),
+    constraint date_container_family_tree_fk foreign key (family_tree_id)
+        references family_tree (family_tree_id)
+);
+
+create table event (
+    event_id            character varying(32),
+    family_tree_id      character varying(32),
+    creation_date       timestamp without time zone,
+    modification_date   timestamp without time zone,
+    date_container_id   character varying(32),
+
+    constraint event_pk primary key (event_id),
+    constraint event_date_container_fk foreign key (date_container_id)
+        references date_container (date_container_id),
+    constraint event_family_tree_fk foreign key (family_tree_id)
+        references family_tree (family_tree_id)
+);
